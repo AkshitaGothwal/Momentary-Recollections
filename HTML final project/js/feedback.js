31796117a41f4b77c9df690827a337ceb72b3823
AkshitaGothwal <149343086+AkshitaGothwal@users.noreply.github.com>
2023-12-08 21:52:13 -0500

    Add files via upload

---
+++ HTML final project/js/feedback.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Add an event listener to the sendButton
+    document.getElementById('sendButton').addEventListener('click', function () {
+        alert('Your feedback has been submitted! Thank you for your valuable opinion.');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Add an event listener to the subscribed button
+    document.getElementById('subscribed').addEventListener('click', function () {
+        alert('You have subscribed! Thank you for your support.');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Add an event listener to the Message button
+    document.getElementById('Message').addEventListener('click', function () {
+        alert('Thank you for your interest for getting knowlegde about our company.');
+    });
+});
